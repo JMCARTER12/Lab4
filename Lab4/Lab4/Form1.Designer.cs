dc586b9e846ac3f2bc9e8eaa78bbb99f8a97e168
JMCARTER12 <82064669+JMCARTER12@users.noreply.github.com>
2021-04-07 15:56:46 -0500

    Add files via upload

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Lab4
+{
+    partial class QualificationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GpaText = new System.Windows.Forms.Label();
+            this.GPAInput = new System.Windows.Forms.TextBox();
+            this.TestScoreInput = new System.Windows.Forms.TextBox();
+            this.AdmissionText = new System.Windows.Forms.Label();
+            this.QualificationOutput = new System.Windows.Forms.Label();
+            this.MainButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AcceptOutput = new System.Windows.Forms.Label();
+            this.RejectOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GpaText
+            // 
+            this.GpaText.Location = new System.Drawing.Point(2, 21);
+            this.GpaText.Name = "GpaText";
+            this.GpaText.Size = new System.Drawing.Size(140, 23);
+            this.GpaText.TabIndex = 0;
+            this.GpaText.Text = "Enter GPA (0.0 - 4.0) :";
+            // 
+            // GPAInput
+            // 
+            this.GPAInput.Location = new System.Drawing.Point(208, 18);
+            this.GPAInput.Name = "GPAInput";
+            this.GPAInput.Size = new System.Drawing.Size(100, 20);
+            this.GPAInput.TabIndex = 1;
+            // 
+            // TestScoreInput
+            // 
+            this.TestScoreInput.Location = new System.Drawing.Point(208, 69);
+            this.TestScoreInput.Name = "TestScoreInput";
+            this.TestScoreInput.Size = new System.Drawing.Size(100, 20);
+            this.TestScoreInput.TabIndex = 2;
+            // 
+            // AdmissionText
+            // 
+            this.AdmissionText.Location = new System.Drawing.Point(2, 72);
+            this.AdmissionText.Name = "AdmissionText";
+            this.AdmissionText.Size = new System.Drawing.Size(187, 23);
+            this.AdmissionText.TabIndex = 3;
+            this.AdmissionText.Text = "Enter admission test score ( 0 - 100 ) :";
+            // 
+            // QualificationOutput
+            // 
+            this.QualificationOutput.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.QualificationOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.QualificationOutput.Location = new System.Drawing.Point(208, 118);
+            this.QualificationOutput.Name = "QualificationOutput";
+            this.QualificationOutput.Size = new System.Drawing.Size(100, 34);
+            this.QualificationOutput.TabIndex = 4;
+            this.QualificationOutput.Click += new System.EventHandler(this.QualificationOutput_Click);
+            // 
+            // MainButton
+            // 
+            this.MainButton.Location = new System.Drawing.Point(12, 98);
+            this.MainButton.Name = "MainButton";
+            this.MainButton.Size = new System.Drawing.Size(170, 54);
+            this.MainButton.TabIndex = 5;
+            this.MainButton.Text = "Do You Qualify?";
+            this.MainButton.UseVisualStyleBackColor = true;
+            this.MainButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Accept Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Reject Count";
+            // 
+            // AcceptOutput
+            // 
+            this.AcceptOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.AcceptOutput.Location = new System.Drawing.Point(80, 178);
+            this.AcceptOutput.Name = "AcceptOutput";
+            this.AcceptOutput.Size = new System.Drawing.Size(62, 23);
+            this.AcceptOutput.TabIndex = 8;
+            // 
+            // RejectOutput
+            // 
+            this.RejectOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.RejectOutput.Location = new System.Drawing.Point(80, 218);
+            this.RejectOutput.Name = "RejectOutput";
+            this.RejectOutput.Size = new System.Drawing.Size(62, 23);
+            this.RejectOutput.TabIndex = 9;
+            // 
+            // QualificationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 260);
+            this.Controls.Add(this.RejectOutput);
+            this.Controls.Add(this.AcceptOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainButton);
+            this.Controls.Add(this.QualificationOutput);
+            this.Controls.Add(this.AdmissionText);
+            this.Controls.Add(this.TestScoreInput);
+            this.Controls.Add(this.GPAInput);
+            this.Controls.Add(this.GpaText);
+            this.Name = "QualificationForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label GpaText;
+        private System.Windows.Forms.TextBox GPAInput;
+        private System.Windows.Forms.TextBox TestScoreInput;
+        private System.Windows.Forms.Label AdmissionText;
+        private System.Windows.Forms.Label QualificationOutput;
+        private System.Windows.Forms.Button MainButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label AcceptOutput;
+        private System.Windows.Forms.Label RejectOutput;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,77 @@
+﻿// Grade ID: R8206
+// Lab # 4
+// Due date: Feb 16 , 2020
+// Section : 01
+// This program determines if someone qualifies for this college by the user given inputs for GPA and test scores.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class QualificationForm : Form
+    {
+        public QualificationForm()
+        {
+            InitializeComponent();
+        }
+        // This event handler calculates Accept or Rejct from a user input of GPA and Test score, with also counting how many accepts and rejects generated
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // declaring variables
+            float GradePointAverage;
+            int TestScore;
+            int AcceptCount = 0;
+            int RejectCount = 0;
+            const int LOW_SCORE = 0;
+            const int HIGH_SCORE = 100;
+            const double LOW_GPA = 0.0;
+            const double HIGH_GPA = 4.0;
+ 
+            float.TryParse(GPAInput.Text, out GradePointAverage); // converting user input using TryParse
+            
+            int.TryParse(TestScoreInput.Text, out TestScore);
+
+            if (TestScore < LOW_SCORE || TestScore > HIGH_SCORE) // sends error messages if user input incorrect GPA or Test score
+            {
+                MessageBox.Show("Enter Valid Test Score");
+            }
+            else if (GradePointAverage < LOW_GPA || GradePointAverage > HIGH_GPA)
+            {
+                MessageBox.Show("Enter Valid GPA");
+            }
+
+            // Determines if the inputs are accept or reject, given the qualifications for accept
+            if ((GradePointAverage >= 3.0 && TestScore >= 60) || (GradePointAverage <= 3.0 && TestScore >= 80))
+            {
+                QualificationOutput.Text = "\nAccept!"; // Output for Accept
+                ++AcceptCount; // Adds to Accept count
+                AcceptOutput.Text = (AcceptCount.ToString(""));
+            }
+            if ((GradePointAverage < 3.0 && TestScore < 60) || (GradePointAverage < 3.0 && TestScore > 80))
+            {
+                QualificationOutput.Text = "\nReject"; // Output for Reject
+                ++RejectCount; // Adds to reject count
+                RejectOutput.Text = (RejectCount.ToString(""));
+            }
+        }
+        
+        
+
+        private void GPAInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QualificationOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
